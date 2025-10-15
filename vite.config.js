678c2ece9743db5ad43da29b91cbd5d1a4c4db01
kharzyme <rizqullah620@gmail.com>
2025-10-15 09:34:45 +0800

    menambah public directory

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            publicDirectory: 'public', 
         }),
         tailwindcss(),
     ],
