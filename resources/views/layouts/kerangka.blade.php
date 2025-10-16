f948457502fde8d508ae69a90c49c6664f6a258c
kharzyme <rizqullah620@gmail.com>
2025-10-16 10:38:05 +0800

    mode lightmode

--- resources/views/layouts/kerangka.blade.php
+++ resources/views/layouts/kerangka.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="color-scheme" content="light only">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Undangan Fulanah & Fulan</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
