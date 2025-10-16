842e900bddf1bd729b02226772497be34a11a9f2
kharzyme <rizqullah620@gmail.com>
2025-10-16 08:24:06 +0800

    coba5

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,19 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     base: '/',
     build: {
         outDir: 'public/build',
         emptyOutDir: true,
-        manifest: true,
-        rollupOptions: {
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            output: {
-                entryFileNames: 'assets/[name].[hash].js',
-                chunkFileNames: 'assets/[name].[hash].js',
-                assetFileNames: 'assets/[name].[ext]',
-            },
-        },
     },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            buildDirectory: 'build', // 💡 ini yang penting
         }),
         tailwindcss(),
     ],
-});
+})
